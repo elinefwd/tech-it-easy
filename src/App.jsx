1d62ec0988f40190f97921a1731e863aab7de114
elinefwd <elinefwd@gmail.com>
2023-11-13 09:02:24 +0100

    first update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+
 import './App.css';
+import Soldtvs from "./assets/helpers/soldtvs.js";
 
 function App() {
   return (
+
     <h1>Begin hier met met maken van de applicatie!</h1>
+      /*<p>{Soldtvs}</p> */
+
   )
 }
 
 export default App
+
